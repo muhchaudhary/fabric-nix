60c7884fe419495d843d5302e1071bac4837fa30
muhchaudhary <muhammadahmchaudhary@gmail.com>
2024-02-14 09:44:08 -0500

    use css to dynamically resize player

--- fabric/bar/bar.py
+++ fabric/bar/bar.py
@@ -26,7 +26,7 @@ from fabric.utils import (
     get_relative_path,
 )
 import gi
-from gi.repository import Gio, GLib, Playerctl, Gtk
+from gi.repository import Gio, GLib, Playerctl, Gtk, GdkPixbuf
 
 PYWAL = False
 AUDIO_WIDGET = True
@@ -87,10 +87,12 @@ class VolumeWidget(Box):
 
 class playerBox(Box):
     def __init__(self, player: Playerctl.Player, **kwargs):
+        # TODO: create a player service
         super().__init__(**kwargs)
         self.player = player
         self.player_width = 350
         self.text_wrap_width = 200
+        self.image_size = 120
         self.player_height = 100
         self.cover_path = ""
         self.old_cover_path = ""
@@ -106,8 +108,8 @@ class playerBox(Box):
             h_align="start",
             v_align="start",
         )
-        self.image_box.set_size_request(120,120)
-        self.last_image_box.set_size_request(120,120)
+        self.image_box.set_size_request(self.image_size,self.image_size)
+        self.last_image_box.set_size_request(self.image_size,self.image_size)
         
         self.image_stack = Stack(
             transition_duration=500,
@@ -145,22 +147,19 @@ class playerBox(Box):
         self.track_info.set_size_request(self.text_wrap_width,-1)
 
         # Buttons 
-
         self.button_box = CenterBox(
             name = "button-box",
-            spacing= 10,
         )
-        
-        self.play_pause_button = Button(name="player-button", child = Image(icon_name="media-playback-start"))
+        # Image(image_file=get_relative_path("assets/svg_logo.svg"))
+        self.play_pause_button = Button(name="player-button", child = Image(image_file = get_relative_path("assets/play.svg")))
         self.play_pause_button.connect("clicked", lambda _: self.player.play_pause())
-        
-        self.next_button = Button(name="player-button", child = Image(icon_name="media-seek-forward"))
+        self.next_button = Button(name="player-button", child = Image(image_file = get_relative_path("assets/skip-next.svg")))
         self.next_button.connect("clicked", lambda _: self.player.next())
 
-        self.prev_button = Button(name="player-button", child = Image(icon_name="media-seek-backward"))
+        self.prev_button = Button(name="player-button", child = Image(image_file = get_relative_path("assets/skip-prev.svg")))
         self.prev_button.connect("clicked", lambda _: self.player.previous())
 
-        self.shuffle_button = Button(name="player-button", child = Image(icon_name="media-playlist-shuffle"))
+        self.shuffle_button = Button(name="player-button", child = Image(image_file=get_relative_path("assets/shuffle.svg")))
         self.shuffle_button.connect("clicked", lambda _: player.set_shuffle(False) if player.get_property("shuffle") else player.set_shuffle(True))
 
         self.button_box.add_center(self.play_pause_button)
@@ -186,8 +185,9 @@ class playerBox(Box):
         self.player.connect("metadata", self.update)
 
         self.player_info_box = Box(
+            style = f"margin-left: {self.image_size + 10}px",
             v_align='center',
-            h_align='end',
+            h_align='start',
             orientation='v',
             spacing=10,
             children=[self.track_info, self.button_box],
@@ -195,31 +195,26 @@ class playerBox(Box):
        
 
         self.inner_box = Box(
-            style="background-color: white; border-radius: 20px;", 
+            style=f"background-color: #FEFEFE; border-radius: 20px; margin-left: {self.image_size // 2};", 
             v_align='center')
+        # resize the inner box 
+        self.inner_box.set_size_request(-1,self.player_height)
+
         self.overlay_box = Box()
         # get a better way to do this later
-        self.overlay_box.set_size_request(self.player_width,self.player_height + 20)
+        self.overlay_box.set_size_request(self.player_width,self.image_size)
         self.overlay_box = Overlay(
             children=self.overlay_box,
             overlays=[
                 Overlay(
                     children=self.inner_box,
-                    overlays=[
-                        self.player_info_box
-                    ],                    
+                    overlays=[self.player_info_box],
                 ),
                 self.image_stack,
             ],
             v_align='center',
             h_align='center'
         )
-        # push the icons a litte from the left
-        self.player_info_box.set_margin_end(20)
-        # hide a bit of the inner_box underneath the image
-        self.inner_box.set_margin_start(10)
-        # resize the inner box 
-        self.inner_box.set_size_request(-1,self.player_height - 10)
 
         self.update(player,player.get_property("metadata"))
         self.playback_update(player,player.get_property("playback-status"))
@@ -232,16 +227,15 @@ class playerBox(Box):
     def shuffle_update(self, player, status):
         logger.info(f"[Player] shuffle status changed to {status}")
         if status == True:
-            self.shuffle_button.get_child().set_from_icon_name("media-playlist-shuffle-symbolic", Gtk.IconSize.BUTTON)
+            self.shuffle_button.get_child().set_from_file(get_relative_path(get_relative_path("assets/shuffle.svg")))
         else:
-            self.shuffle_button.get_child().set_from_icon_name("media-playlist-shuffle", Gtk.IconSize.BUTTON)
+            self.shuffle_button.get_child().set_from_file(get_relative_path(get_relative_path("assets/shuffle-gray.svg")))
 
     def playback_update(self, player, status):
         if status == Playerctl.PlaybackStatus.PAUSED:
-            self.play_pause_button.get_child().set_from_icon_name("media-playback-start", Gtk.IconSize.DND)
+            self.play_pause_button.get_child().set_from_file(get_relative_path(get_relative_path("assets/play.svg")))
         if status == Playerctl.PlaybackStatus.PLAYING:
-            self.play_pause_button.get_child().set_from_icon_name("media-playback-pause", Gtk.IconSize.DND)
-            print(status)
+            self.play_pause_button.get_child().set_from_file(get_relative_path(get_relative_path("assets/pause.svg")))
 
         logger.info(f"[PLAYER] status changed to {status}")
 
@@ -256,7 +250,7 @@ class playerBox(Box):
 
     def update_image(self):
         self.image_box.set_style(style=f"background-image: url('{self.cover_path}'); background-size: cover;")
-        #self.container_box.set_style(style=f"background-image: url('{self.cover_path}'); background-size: cover;")
+        #self.inner_box.set_style(style=f"background-image: url('{self.cover_path}'); background-size: cover;")
         self.last_image_box.set_style(style=f"background-image: url('{self.old_cover_path}'); background-size: cover;")
         self.image_stack.set_visible_child_name("last_player_image")
         self.image_stack.set_visible_child_name("player_image")
@@ -281,22 +275,13 @@ class playerBox(Box):
         )
 
     def update(self, player, metadata):
-        logger.info(f"[PLAYER] new song")
-        self.seek_bar.set_adjustment(Gtk.Adjustment(0, 0, metadata['mpris:length'], 2e6, 2e6, 0))
-        self.set_image(metadata['mpris:artUrl'])
+        logger.info(f"[PLAYER] new song {metadata}")
+        #self.seek_bar.set_adjustment(Gtk.Adjustment(0, 0, metadata['mpris:length'], 2e6, 2e6, 0))
+        if 'mpris:artUrl' in metadata.keys():
+            self.set_image(metadata['mpris:artUrl'])
         self.track_title.set_label(metadata['xesam:title'])
         self.track_artist.set_label(metadata["xesam:artist"][0])
 
-
-class playerWidget(Box):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.mpris_manager = MprisPlayerManager()
-
-        self.player_info_label = Label(label="")
-
-    def get_players(self):
-        return self.mpris_manager.get_players()
 
 class StatusBar(Window):
     def __init__(
@@ -368,7 +353,9 @@ class StatusBar(Window):
             ],
         )
         self.volume = VolumeWidget() if AUDIO_WIDGET is True else None
-        self.mprisplayer = playerWidget()
+        self.mprisplayer = MprisPlayerManager()
+        self.mprisplayer.connect('player-appeared',self.on_new_player)
+        #self.mprisplayer.connect('player-vanished', self.on_lost_player)
         self.player_box = playerBox(self.mprisplayer.get_players()[0])
         self.widgets_container = Box(
             spacing=2,
@@ -392,6 +379,13 @@ class StatusBar(Window):
         self.update_progress_bars()  # initial call
 
         self.show_all()
+    
+    def on_new_player(self, mpris_manager, player_manager, player,):
+        logger.info(f"{player_manager}, {player}")
+        self.center_box.add_left(playerBox(player=player))
+    
+    # def on_lost_player(self, player_manager, player):
+    #     self.center_box.remove_left()
 
     def update_progress_bars(self):
         self.ram_circular_progress_bar.percentage = psutil.virtual_memory().percent
