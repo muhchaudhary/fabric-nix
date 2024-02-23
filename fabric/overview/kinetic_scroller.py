e7dc809a6a52e9195e314261606beb60363c9eea
muhammadahmchaudhary@gmail.com <muhchaudhary>
2024-02-22 22:47:28 -0500

    update kinetic scrolling

--- fabric/overview/kinetic_scroller.py
+++ fabric/overview/kinetic_scroller.py
@@ -2,24 +2,26 @@
 
 from fabric.widgets.eventbox import EventBox
 from fabric.utils import clamp, invoke_repeater
-from fabric.service import *
+from fabric.service import Signal, SignalContainer
 import math
+from loguru import logger
 
 class KineticScroll(EventBox):
     __gsignals__ = SignalContainer(
         Signal("smooth-scroll-event", "run-first", None, (float,)),
         Signal("normal-scroll-event", "run-first", None, (float,))
     )
-    def __init__(self, mult: float, min_value: int, max_value: int, **kwargs):
+    def __init__(self, multiplier: float, min_value: int, max_value: int, **kwargs):
         super().__init__(
             # TODO add normal scrolling later
             events="smooth-scroll",
             **kwargs
         )
-        self._mult = mult
+        self._mult = multiplier
         self._min = min_value
         self._max = max_value
         self.scroll_value = 0
+        self.is_scrolling = False
 
         # Samples
         self.n_samples = 4
@@ -28,13 +30,13 @@ class KineticScroll(EventBox):
 
     #currently will only handle smooth scroll events
     def on_scroll_event(self, widget, event):
+        self.is_scrolling = True
         val_y = event.delta_y
         if val_y != 0:
             adj_delta = val_y * self._mult
             self.scroll_value += adj_delta
             self.scroll_value = clamp(self.scroll_value, self._min, self._max)
 
-            # TODO: create a new smooth-scroll-event signal
             self.emit("smooth-scroll-event", self.scroll_value)
 
             # use a circular buffer type structure, this sucks
@@ -43,19 +45,33 @@ class KineticScroll(EventBox):
         else:
             # invoke repeater to run the kinetic scroll simulation here
             self.ticks = 0
-            print("Velocity: ", sum(self.samples) / self.n_samples)
-            invoke_repeater(1, self.on_scroll_event_end, sum(self.samples) / self.n_samples)
+            logger.info(f"Velocity: {sum(self.samples) / self.n_samples}")
+            self.is_scrolling = False
+            invoke_repeater(8, self.on_scroll_event_end, sum(self.samples) / self.n_samples)
 
     # kinetic scroll simulation
     def on_scroll_event_end(self, velocity):
-        new_velocity = self.easing_funct(abs(velocity),self.ticks)
-        if self.scroll_value + new_velocity >= self._max:
+        if self.is_scrolling:
             return False
-        self.scroll_value += new_velocity if velocity > 0 else -new_velocity
-        print(new_velocity)
-        self.ticks += 0.001
-        self.emit("smooth-scroll-event", self.scroll_value)
-        return True if abs(new_velocity) > 0.001 else False
 
-    def easing_funct(self, velocity, time):
+        new_velocity = self.easing_function(abs(velocity),self.ticks)
+
+        if self.scroll_value + new_velocity >= self._max:
+            self.scroll_value = self._max
+            self.emit("smooth-scroll-event", self.scroll_value)
+            return False
+
+        if self.scroll_value - new_velocity <= self._min:
+            self.scroll_value = self._min
+            self.emit("smooth-scroll-event", self.scroll_value)
+            return False
+        print(new_velocity, self.scroll_value)
+        self.scroll_value += new_velocity if velocity > 0 else -new_velocity
+        self.ticks += 0.008
+        self.emit("smooth-scroll-event", self.scroll_value)
+
+        return True if abs(new_velocity) > 0.2 else False
+
+    def easing_function(self, velocity, time):
         return velocity * math.exp(-2*(1/velocity) * time)
+        # return -1/3 * time + velocity
--- fabric/overview/overview.py
+++ fabric/overview/overview.py
@@ -1,21 +1,15 @@
-import json
 import fabric
-import os
 from loguru import logger
 from fabric.widgets.box import Box
 from fabric.widgets.wayland import Window
-from fabric.widgets.eventbox import EventBox
+from fabric.widgets.overlay import Overlay
 from fabric.widgets.centerbox import CenterBox
 from kinetic_scroller import KineticScroll
 from fabric.widgets.scale import Scale
 from fabric.utils import (
     set_stylesheet_from_file,
     get_relative_path,
-    invoke_repeater,
-    clamp,
 )
-from gi.repository import Gdk
-
 class Overview(Window):
     def __init__(
         self,
@@ -30,21 +24,25 @@ class Overview(Window):
         self.iters = 0
 
         self.scroller = Scale(min_value=0,
-                              max_value=100,
+                              max_value=1000,
                               orientation="h",
                               draw_value=False,
                               name="seek-bar")
-        self.scroller.set_size_request(500, -1)
+        self.scroller.set_size_request(800, -1)
         self.scrollBox = Box(name="box",
                              children=self.scroller,
+                             style= "padding: 50px"
                              )
         # self.workspacescroll = EventBox(style="background-color: black;",events="smooth-scroll", )
+
         self.workspacescroll = KineticScroll(
-            mult=2,
+            style = "background-color: rgba(0,0,0,0.25);",
+            multiplier=10,
             min_value=0,
-            max_value=100,
-            children=CenterBox(orientation="v",center_widgets=Box(size=100, style="background-color: red;") ,left_widgets=self.scrollBox)
+            max_value=1000,
+            children=CenterBox(orientation="v",center_widgets=[self.scrollBox,Box(size=100)])
         )
+        self.scroller.connect("scroll-event", self.on_scroll)
         self.workspacescroll.connect("smooth-scroll-event", self.on_scroll)
 
         self.add(self.workspacescroll)
