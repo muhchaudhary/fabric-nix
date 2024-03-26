64bf8adc04289fc30630eab9623ab897788f76a2
muhammadahmchaudhary@gmail.com <muhchaudhary>
2024-03-25 23:44:35 -0400

    allow timout to be given for how long popup will appear

--- fabric/widgets/popup_window.py
+++ fabric/widgets/popup_window.py
@@ -3,25 +3,23 @@ from fabric.widgets.revealer import Revealer
 from fabric.widgets.label import Label
 from fabric.widgets.box import Box
 
+from gi.repository import GLib
 
 
 class PopupWindow(Window):
     def __init__(
         self,
-        child,
-        transition_type: str,
-        transition_duration: int,
+        child = None,
+        transition_type: str = "",
+        transition_duration: int = 100,
         visible: bool = False,
         anchor: str = "top right",
         *kwargs,
     ):
-        super().__init__(
-            layer="overlay",
-            anchor=anchor,
-            all_visible=True,
-            exclusive=False,
-            *kwargs
-        )
+
+        self.timeout = 1000
+        self.currtimeout = 0
+        self.popup_running = False
 
         self.revealer = Revealer(
             transition_type=transition_type,
@@ -30,9 +28,38 @@ class PopupWindow(Window):
         )
         self.box = Box(style="padding:1px;", children=self.revealer)
         self.visible = visible
-        self.add(self.box)
+        super().__init__(
+            layer="overlay",
+            anchor=anchor,
+            all_visible=True,
+            exclusive=False,
+            children=self.box,
+            *kwargs
+        )
+
         self.show_all()
 
     def toggle_popup(self):
         self.visible = not self.visible
         self.revealer.set_reveal_child(self.visible)
+
+    def popup_timeout(self):
+        if self.popup_running:
+            self.currtimeout = 0
+            return
+        self.visible = True
+        self.revealer.set_reveal_child(True)
+        def popup_func():
+            if self.currtimeout > self.timeout:
+                self.visible = False
+                self.revealer.set_reveal_child(False)
+                self.currtimeout = 0
+                self.popup_running = False
+                return False
+            self.popup_running = True
+            self.currtimeout += 50
+            return True
+        GLib.timeout_add(100, popup_func)
+
+    def popup_rest(self):
+        self.currtimeout = 0
