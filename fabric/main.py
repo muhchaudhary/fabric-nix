af32097d03a4cbf8d736e815d18f266162f0eace
muhammadahmchaudhary@gmail.com <muhchaudhary>
2024-03-25 23:41:04 -0400

    remove pywal, add audio OSD popip

--- fabric/main.py
+++ fabric/main.py
@@ -1,18 +1,13 @@
 import fabric
-import os
 from fabric.utils import set_stylesheet_from_file, get_relative_path, monitor_file
 from components.bar.bar import StatusBar
 from components.desktop.desktop_widget import ClockWidget
-from widgets.popup_window import PopupWindow
+from components.quick_settings.quick_settings import AudioOverlay
 import gi
 
-gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk  # noqa: E402
 
 # from overview.overview import Overview
 from loguru import logger
-
-PYWAL = True
 
 
 def apply_style(*args):
@@ -25,14 +20,8 @@ if __name__ == "__main__":
     logger.disable("fabric.audio.service")
     bar = StatusBar()
     clockWidget = ClockWidget()
-    # overview = Overview()
-    # bt = BluetoothWidget()
+    audioOverlay = AudioOverlay()
 
-    if PYWAL is True:
-        monitor = monitor_file(get_relative_path("style/main.css"))
-        monitor.connect("changed", apply_style)
-
-    # initialize style
     apply_style()
 
     fabric.start()
