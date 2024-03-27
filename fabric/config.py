60471d41a954c56e032c5b9fe8fa8781d2a4feb4
muhammadahmchaudhary@gmail.com <muhchaudhary>
2024-03-27 08:44:17 -0400

    move all services, icons, global vars into config file

---
+++ fabric/config.py
@@ -0,0 +1,50 @@
+from services.mpris import MprisPlayerManager
+from services.screen_record import ScreenRecorder
+from fabric.audio.service import Audio
+from fabric.bluetooth.service import BluetoothClient
+
+
+# Services
+mprisplayer = MprisPlayerManager()
+bluetooth_client = BluetoothClient()
+audio = Audio()
+sc = ScreenRecorder()
+
+# Global variables
+inhibit_overlay = False
+
+# Icons
+bluetooth_icons = {
+    "bluetooth": "󰂯",
+    "bluetooth-off": "󰂲",
+}
+
+audio_icons = {"mute": "󰝟", "off": "󰖁", "high": "󰕾", "low": "󰕿", "medium": "󰖀"}
+
+battery_icons = {
+    0: "󱃍",
+    10: "󰁺",
+    20: "󰁻",
+    30: "󰁼",
+    40: "󰁽",
+    50: "󰁾",
+    60: "󰁿",
+    70: "󰂀",
+    80: "󰂁",
+    90: "󰂂",
+    100: "󰁹",
+}
+
+battery_charging_icons = {
+    0: "󰢟",
+    10: "󰢜",
+    20: "󰂆",
+    30: "󰂇",
+    40: "󰂈",
+    50: "󰢝",
+    60: "󰂉",
+    70: "󰢞",
+    80: "󰂊",
+    90: "󰂋",
+    100: "󰂅",
+}
