e8dd0d0e4d1850c3f72ecaa8a9f121697498b724
muhchaudhary <61593188+muhchaudhary@users.noreply.github.com>
2024-10-03 15:00:41 -0400

    feat: add a calendar tooltip

--- fabric/components/bar/widgets/date_time.py
+++ fabric/components/bar/widgets/date_time.py
@@ -2,6 +2,21 @@ from gi.repository import GLib
 
 from fabric.widgets.button import Button
 
+import calendar
+from datetime import date
+
+
+class DayInMonthHighlightingCalendar(calendar.TextCalendar):
+    def __init__(self, day_to_highlight):
+        super().__init__()
+        self._day_to_highlight = day_to_highlight
+
+    def formatday(self, day: int, weekday: int, width: int) -> str:
+        s = super().formatday(day, weekday, width)
+        if day == self._day_to_highlight:
+            s = f"<span foreground='green'>{s}</span>"
+        return s
+
 
 class DateTime(Button):
     def __init__(self, **kwargs):
@@ -10,6 +25,14 @@ class DateTime(Button):
         self.format = "%a %b %d  %I:%M %p"
         self.update_label()
         GLib.timeout_add(self.interval, self.update_label)
+
+        today = date.today()
+        year, month, day, *_ = today.timetuple()
+        self.set_tooltip_markup(
+            DayInMonthHighlightingCalendar(day_to_highlight=day).formatmonth(
+                year, month, 3
+            )
+        )
 
     def update_label(self, *args):
         self.set_label(GLib.DateTime.new_now_local().format("%a %b %d  %I:%M %p"))
