104b88d5ac655e8e2d391276ab78f9c3613b1f55
muhchaudhary <61593188+muhchaudhary@users.noreply.github.com>
2024-10-07 02:57:49 -0400

    add documentation for hyprland_monitor

--- fabric/utils/hyprland_monitor.py
+++ fabric/utils/hyprland_monitor.py
@@ -10,14 +10,26 @@ from fabric.hyprland import Hyprland
 gi.require_version("Gdk", "3.0")
 from gi.repository import Gdk
 
-# IDC,  screen.get_monitor_plug_name is deprecated
+# IDC,  Gdk.Screen.get_monitor_plug_name is deprecated
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+# Another idea is to use Gdk.Monitor.get_model() however,
+#       there is no garuntee that this will be unique
+#       Example: both monitors have the same model number
+#       (quite common in multi monitor setups)
+
+
+# Also, using Gdk.Display.get_monitor_at_point(x,y)
+#       does not work correctly on all wayland setups
+
+
+# Annoyingly, Gdk 4.0 has a solution to this with
+#       Gdk.Monitor.get_description() or Gdk.Monitor.get_connector()
+#       which both can be used to uniquely identify a monitor
 
 class HyprlandWithMonitors(Hyprland):
     def __init__(self, commands_only: bool = False, **kwargs):
         self.display: Gdk.Display = Gdk.Display.get_default()
-        self.screen: Gdk.Screen = Gdk.Display.get_default().get_default_screen()
         super().__init__(commands_only, **kwargs)
 
     # Add new arguments
@@ -27,7 +39,7 @@ class HyprlandWithMonitors(Hyprland):
 
     def get_gdk_monitor_id_from_name(self, plug_name: str) -> int | None:
         for i in range(self.display.get_n_monitors()):
-            if self.screen.get_monitor_plug_name(i) == plug_name:
+            if self.display.get_default_screen().get_monitor_plug_name(i) == plug_name:
                 return i
         return None
 
