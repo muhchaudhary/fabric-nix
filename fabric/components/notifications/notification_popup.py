7bb2026adf5833d8ccf5a5367d77784d3ddac773
muhchaudhary <61593188+muhchaudhary@users.noreply.github.com>
2024-10-20 21:03:36 -0400

    feat: rework notifications

--- fabric/components/notifications/notification_popup.py
+++ fabric/components/notifications/notification_popup.py
@@ -19,28 +19,15 @@ from gi.repository import AstalNotifd  # noqa: E402
 # TODO: group notifications by type
 
 
-class NotificationBox(Box):
+class NotificationBox(CenterBox):
     def __init__(self, notification: AstalNotifd.Notification):
-        self.app_body = notification.get_body()
-        self.app_title = notification.get_summary()
-
-        self.action_buttons = [
-            Button(
-                label=action.label,
-                on_clicked=lambda *_: notification.invoke(action.id),
-                h_expand=True,
-            )
-            for action in notification.get_actions()
-        ]
-
-        logger.error(f"APP ICON: {notification.get_app_icon()}")
-
-        self.notification_box = CenterBox(
+        super().__init__(
             name="notification-box",
             orientation="v",
             start_children=[
                 Box(
-                    spacing=5,
+                    name="notification-title",
+                    spacing=0,
                     children=[
                         Image(
                             icon_name=notification.get_app_icon()
@@ -85,22 +72,32 @@ class NotificationBox(Box):
             end_children=[
                 Box(
                     name="notification-action-buttons",
-                    children=self.action_buttons,
+                    children=[
+                        Button(
+                            label=action.label,
+                            on_clicked=lambda *_: notification.invoke(action.id),
+                            h_expand=True,
+                        )
+                        .build()
+                        .add_style_class(f"id{i}")
+                        .unwrap()
+                        for i, action in enumerate(notification.get_actions())
+                    ],
                     h_expand=True,
                 )
             ],
         )
-
-        super().__init__(children=self.notification_box)
 
 
 class NotificationRevealer(Revealer):
     def __init__(self, notification: AstalNotifd.Notification, **kwargs):
         self.popup_timeout = 5000
         super().__init__(
-            child=NotificationBox(notification),
+            child=Box(
+                style="margin: 1px 0px 1px 1px;", children=NotificationBox(notification)
+            ),
             transition_duration=500,
-            transition_type="slide-right",
+            transition_type="crossfade",
         )
         invoke_repeater(self.popup_timeout, lambda *_: self.set_reveal_child(False))
         self.connect(
@@ -131,7 +128,9 @@ class NotificationPopup(WaylandWindow):
         self.pop_up_list = []
         self._server.astal_notifd.connect("notified", self.on_new_notification)
         self.notifications = Box(
-            style="padding:1px;",
+            v_expand=True,
+            h_expand=True,
+            style="margin: 1px 0px 1px 1px;",
             orientation="v",
             spacing=5,
         )
@@ -143,11 +142,8 @@ class NotificationPopup(WaylandWindow):
             visible=True,
             exclusive=False,
         )
-        self.show_all()
 
     def on_new_notification(self, astal_notifd: AstalNotifd.Notifd, id: int, idk):
         new_box = NotificationRevealer(astal_notifd.get_notification(id))
-        self.notifications.add(
-            Box(style="padding: 1px", children=new_box, h_align="end")
-        )
+        self.notifications.add(new_box)
         new_box.set_reveal_child(True)
