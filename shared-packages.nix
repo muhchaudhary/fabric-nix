428f954df854a4c95a24c36ae429c30d276d47b3
muhchaudhary <61593188+muhchaudhary@users.noreply.github.com>
2025-06-09 22:09:26 -0400

    add shared-packages.nix back for now

--- shared-packages.nix
+++ shared-packages.nix
@@ -1,19 +1,18 @@
-# # shared-packages.nix
-# {pkgs}: {
-#   sharedPythonPackages = with pkgs.python3Packages;
-#     [
-#       psutil
-#       requests
-#       lxml
-#       pam
-#       thefuzz
-#       numpy
-#       magic
-#     ]
-#     ++ [
-#       # custom python packages
-#       # pkgs.rlottie-python
-#       pkgs.gengir
-#       pkgs.pywayland-custom
-#     ];
-# }
+# shared-packages.nix
+{pkgs}: {
+  sharedPythonPackages = with pkgs.python3Packages;
+    [
+      psutil
+      requests
+      lxml
+      pam
+      thefuzz
+      numpy
+      magic
+      colorthief
+    ]
+    ++ [
+      pkgs.gengir
+      pkgs.pywayland-custom
+    ];
+}
