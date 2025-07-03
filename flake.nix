35c5e371f6154f48208c29a6c1f08f3ec603ad60
muhchaudhary <61593188+muhchaudhary@users.noreply.github.com>
2025-07-03 01:44:48 -0400

    rework flake

--- flake.nix
+++ flake.nix
@@ -4,19 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
-
-    fabric = {
-      url = "github:Fabric-Development/fabric";
-      # inputs.nixpkgs.follows = "nixpkgs";
-    };
-    fabric-libgray = {
-      url = "github:Fabric-Development/gray";
-      # inputs.nixpkgs.follows = "nixpkgs";
-    };
-    fabric-libglace = {
-      url = "github:muhchaudhary/glace/hyprland";
-      # inputs.nixpkgs.follows = "nixpkgs";
-    };
+    fabric.url = "github:Vortriz/fabric";
+    fabric-libgray.url = "github:Fabric-Development/gray";
+    fabric-libglace.url = "github:muhchaudhary/glace/hyprland";
   };
 
   outputs = {
@@ -29,18 +19,17 @@
     utils.lib.eachDefaultSystem (
       system: let
         overlays = [
-          (final: prev: {fabric-libglace = inputs.fabric-libglace.packages.${system}.default;})
-          (final: prev: {basedpyright = nixpkgs.legacyPackages.${system}.basedpyright;})
-          (final: prev: {fabric-libgray = inputs.fabric-libgray.packages.${system}.default;})
-
+          (final: prev: {
+            fabric-libglace = inputs.fabric-libglace.packages.${system}.default;
+            basedpyright = nixpkgs.legacyPackages.${system}.basedpyright;
+            fabric-libgray = inputs.fabric-libgray.packages.${system}.default;
+          })
           (final: prev: {
             gengir = pkgs.python3Packages.callPackage ./nix/gengir.nix {
               typer = pkgs.python3Packages.typer;
               astor = pkgs.python3Packages.astor;
               lxml = pkgs.python3Packages.lxml;
             };
-          })
-          (final: prev: {
             rlottie-python = pkgs.python3Packages.callPackage ./nix/rolttie-python.nix {
               distlib = pkgs.python3Packages.distlib;
               flit-core = pkgs.python3Packages.flit-core;
@@ -48,29 +37,22 @@
               click = pkgs.python3Packages.click;
             };
           })
-          (final: prev: {pywayland-custom = pkgs.python3Packages.callPackage ./nix/pywayland.nix {};})
-          (final: prev: {run-widget = fabric.packages.${system}.run-widget;})
-
-          fabric.overlays.${system}.default
+          fabric.overlays.default
         ];
 
         pkgs = import nixpkgs {
-          system = system;
-          overlays = overlays;
+          inherit system overlays;
         };
-        python-depends = {
-          lxml = pkgs.python3Packages.lxml;
-          psutil = pkgs.python3Packages.psutil;
-          python-fabric = pkgs.python3Packages.python-fabric;
-          requests = pkgs.python3Packages.requests;
-          thefuzz = pkgs.python3Packages.thefuzz;
-          pam = pkgs.python3Packages.pam;
-          colorthief = pkgs.python3Packages.colorthief;
+
+        python-depends = with pkgs.python3Packages; {
+          inherit lxml psutil requests pam colorthief thefuzz;
+          python-fabric = fabric.packages.${system}.python-fabric;
+          pywayland-custom = pkgs.python3Packages.callPackage ./nix/pywayland.nix {};
         };
       in {
         formatter = pkgs.nixfmt-rfc-style;
         devShells.default = pkgs.callPackage ./shell.nix {
-          inherit pkgs;
+          inherit pkgs python-depends;
         };
         packages.default = pkgs.python3Packages.callPackage ./derivation.nix {
           inherit (pkgs) lib python-depends;
--- shell.nix
+++ shell.nix
@@ -1,50 +1,48 @@
-{pkgs ? import <nixpgs> {}}: let
-  shared = import ./shared-packages.nix {inherit pkgs;};
-in
-  pkgs.mkShell {
-    name = "fabric-shell";
-    packages = with pkgs; [
-      ruff # Linter
-      basedpyright # Language server
+{
+  pkgs,
+  python-depends,
+}:
+pkgs.mkShell {
+  name = "fabric-shell";
+  packages = with pkgs; [
+    ruff # Linter
+    basedpyright # Language server
 
-      # Required for Devshell
-      gtk3
-      gtk-layer-shell
-      cairo
-      gobject-introspection
-      libdbusmenu-gtk3
-      gdk-pixbuf
-      gnome-bluetooth
-      cinnamon-desktop
+    # Required for Devshell
+    gtk3
+    gtk-layer-shell
+    cairo
+    gobject-introspection
+    libdbusmenu-gtk3
+    gdk-pixbuf
+    gnome-bluetooth
+    cinnamon-desktop
 
-      # Addional packages
+    # Addional packages
+    fabric-libgray
+    fabric-libglace
+    networkmanager
+    playerctl
+    librsvg
 
-      fabric-libgray
-      fabric-libglace
-      networkmanager
-      playerctl
-      librsvg
+    (python3.withPackages (
+      ps:
+        with ps;
+          [
+            setuptools
+            wheel
+            build
+            pyopengl
+            numpy
+            pygobject-stubs
+          ]
+          ++ (builtins.attrValues python-depends)
+    ))
+  ];
 
-      (python3.withPackages (
-        ps:
-          with ps;
-            [
-              setuptools
-              wheel
-              build
-              python-fabric
-              # TESTING
-              pyopengl
-              numpy
-              pygobject-stubs
-            ]
-            ++ shared.sharedPythonPackages
-      ))
-    ];
-
-    shellHook = ''
-      # ${pkgs.python3.interpreter} ./nix/tt.py
-      # cp -rn "${pkgs.python3Packages.pygobject-stubs}/lib/python3.12/site-packages/gi-stubs/repository/." "/home/$USER/.local/lib/python3.12/site-packages/gi/repository/"
-      export GDK_PIXBUF_MODULEDIR=${pkgs.librsvg}/lib/gdk-pixbuf-2.0/2.10.0/loaders
-    '';
-  }
+  shellHook = ''
+    # ${pkgs.python3.interpreter} ./nix/tt.py
+    # cp -rn "${pkgs.python3Packages.pygobject-stubs}/lib/python3.12/site-packages/gi-stubs/repository/." "/home/$USER/.local/lib/python3.12/site-packages/gi/repository/"
+    export GDK_PIXBUF_MODULEDIR=${pkgs.librsvg}/lib/gdk-pixbuf-2.0/2.10.0/loaders
+  '';
+}
